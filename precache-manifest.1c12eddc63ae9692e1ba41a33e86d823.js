4ed98f981faf2a333cb4bcde18a7c7e65727db61
Nitsua28 <achan113@ucmerced.edu>
2023-01-31 15:40:11 -0800

    Deploy React app to GitHub Pages

---
+++ precache-manifest.1c12eddc63ae9692e1ba41a33e86d823.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c8f0a535d966aca7778",
+    "url": "/90s-website-v2/static/js/main.7c8f0a53.chunk.js"
+  },
+  {
+    "revision": "8654e7e83f475fb519e8",
+    "url": "/90s-website-v2/static/js/1.8654e7e8.chunk.js"
+  },
+  {
+    "revision": "dbd6b977905054fde039",
+    "url": "/90s-website-v2/static/js/runtime~main.dbd6b977.js"
+  },
+  {
+    "revision": "b413ce607b73aa13dc4c6132b9176008",
+    "url": "/90s-website-v2/index.html"
+  }
+];
